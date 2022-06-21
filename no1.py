b70e6b8979bd5f1d3865d7bf9f91837521cbe105
Bagus2605 <bagusmaulidan75@gmail.com>
2022-06-21 14:33:59 +0800

    mengubah else karna jika input 4 muncul data 3

--- no1.py
+++ no1.py
@@ -17,26 +17,33 @@ dataMahasiswa = [
 
 ]
 
-print("Silahkan Pilih Nama Mahasiswa Berikut : ")
-num = 1
-while num < len(dataMahasiswa):
-    for data in dataMahasiswa:
-        print(num, data["Nama"])
-        num += 1
+status = True
+while status:
+    print("Silahkan Pilih Nama Mahasiswa Berikut : ")
+    num = 1
+    while num < len(dataMahasiswa):
+        for data in dataMahasiswa:
+            print(num, data["Nama"])
+            num += 1
 
-input = input("\nMasukkan Angka 1-3 :")
-
-
-def tampil(indeks):
-    print("Nama = ", dataMahasiswa[indeks]['Nama'])
-    print("Nim = ", dataMahasiswa[indeks]['NIM'])
-    print("Jurusan = ", dataMahasiswa[indeks]['Jurusan'])
+    input = input("\nMasukkan Angka 1-3 :")
 
 
-if input == '1':
-    tampil(0)
+    def tampil(indeks):
+        print("Nama = ", dataMahasiswa[indeks]['Nama'])
+        print("Nim = ", dataMahasiswa[indeks]['NIM'])
+        print("Jurusan = ", dataMahasiswa[indeks]['Jurusan'])
 
-elif input == '2':
-    tampil(1)
-else:
-    tampil(2)
+
+    if input == '1':
+        tampil(0)
+
+    elif input == '2':
+        tampil(1)
+    elif input == '2':
+        tampil(2)
+    else:
+        print("Hanya dapat memasukkan Angka 1-3")
+        cek = input("Apakah Anda Ingin Melanjutkan Program? (Ya/No) : ")
+        if cek == 'No':
+            status = False
